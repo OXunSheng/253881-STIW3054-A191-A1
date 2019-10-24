4a4c1f6a17f97e062583222088e8e45842572024
OXunSheng <38216203+OXunSheng@users.noreply.github.com>
2019-10-24 23:28:36 +0800

    Add files via upload

---
+++ src/main/java/com/xunsheng/Unknown.java
@@ -0,0 +1,22 @@
+package com.xunsheng;
+
+import java.util.ArrayList;
+
+public class Unknown {
+
+    private final ArrayList<Data> unknown = new ArrayList<Data>();
+
+    //Create a new ArrayList for students who submitted but does not exist in the list of student.
+    public void makeNewArrayList(ArrayList<Data> wiki, ArrayList<Integer>index){
+        for(int i =0; i<index.size();i++){
+            unknown.add(new Data(wiki.get(index.get(i)).getMatric(), wiki.get(index.get(i)).getName(), wiki.get(index.get(i)).getLink()));
+            unknown.get(i).setNum(i+1);
+        }
+
+
+    }
+
+    public ArrayList<Data> arrayList(){
+        return unknown;
+    }
+}
